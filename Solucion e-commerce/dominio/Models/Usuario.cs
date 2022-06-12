12114b01c82ba1437730f08738307dab8063da32
Analia <ana.lau.villarreal@gmail.com>
2022-06-11 22:33:06 -0300

    con dominio y negocio

---
+++ Solucion e-commerce/dominio/Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dominio
+{
+    public class Usuario
+    {
+    
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Contrasenia { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string email { get; set; }
+        public int Roles { get; set; }
+        public int IdDireccion { get; set; }
+    }
+}
