476476a9cc2a2ef778dd23fbde5e8cad92b7bcab
Analia <ana.lau.villarreal@gmail.com>
2022-06-11 22:41:38 -0300

    cambios

---
+++ Solucion e-commerce/dominio/Models/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string URLImagen { get; set; }
+        public int IDTipo { get; set; }
+        public int IDCategoria { get; set; }
+        public int IDSubcategoria { get; set; }
+        public int IDmarca { get; set; }
+        public int Temporada { get; set; }
+        public int Descuento { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
