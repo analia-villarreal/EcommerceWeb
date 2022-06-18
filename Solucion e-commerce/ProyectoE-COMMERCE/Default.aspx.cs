07423383d123e47227b7932b9e71293d0b7d1d43
lvingman <45737515+lvingman@users.noreply.github.com>
2022-06-17 21:12:37 -0300

    Agrega catalogo a la home
    Falta arreglar el bootstrap, no muestra las cartas como las deberia mostrar

--- Solucion e-commerce/ProyectoE-COMMERCE/Default.aspx.cs
+++ Solucion e-commerce/ProyectoE-COMMERCE/Default.aspx.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using dominio;
+using negocio;
 
 namespace ProyectoE_COMMERCE
 {
+    
+
     public partial class _Default : Page
     {
+        public List<dominio.Articulo> listaArticulos { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            listaArticulos = negocio.Listar();
 
         }
     }
